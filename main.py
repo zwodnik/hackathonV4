a362a42761b8822c2d396328561643b04f0fb90e
Kamil Jarosinski <kamil.jarosinski@gmail.com>
2017-04-07 21:33:05 +0000

    Python table main hub

---
+++ main.py
@@ -0,0 +1,35 @@
+import subprocess
+from table import Table
+import re
+
+import time
+
+
+def display(idT,table):
+	print("ID: {0}".format(idT))
+	print("isDirty: {0}".format(table.isDirty()))
+	print("isBusy: {0}".format(table.isBusy()))
+	print("")
+
+
+list_tables = []
+
+list_tables.append(Table())
+list_tables.append(Table())
+
+while True:
+	try:
+		a = subprocess.check_output("./transfer")
+		ret = str(a)
+		ret = re.findall('.',ret)
+	
+		idTable = int(ret[0])-1
+		#print(list_tables[int(ret[0])])
+		list_tables[idTable].update(int(ret[1]))
+
+		display(idTable,list_tables[idTable])
+		time.sleep(1)
+	except:
+		pass
+
+
